6a484b1f84c657f0baf38c4e5b3e1e0dc066f661
Pranaya Shrestha <pranaya.shrestha2@gmail.com>
2023-02-22 21:28:24 -0600

    removed the full_name field

--- app/graphql/types/artist_type.rb
+++ app/graphql/types/artist_type.rb
@@ -6,12 +6,7 @@ module Types
     field :first_name, String, null: true
     field :last_name, String, null: true
     field :email, String, null: true
-    field :full_name, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-  end
-
-  def full_name
-    [object.first_name, object.last_name].compact.join("")
   end
 end
